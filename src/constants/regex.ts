3775ae031917a32118e35939fbf5343ba104fdc5
Vadym Halahan <galagan.vadim20@gmail.com>
2024-06-10 17:11:33 +0300

    remove lowercase letter validation

--- src/constants/regex.ts
+++ src/constants/regex.ts
@@ -1,6 +1,6 @@
 export const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-export const UPPERCASE_AND_LOWERCASE_REGEX = /^(?=.*[a-z])(?=.*[A-Z]).*$/;
+export const AT_LEAST_ONE_UPPERCASE_REGEX = /^(?=.*[A-Z]).*$/;
 
 export const AT_LEAST_ONE_NUMBER_REGEX = /^(?=.*\d).*$/;
 
--- src/pages/Authorization/hooks/useAuthorizationSchema.ts
+++ src/pages/Authorization/hooks/useAuthorizationSchema.ts
@@ -2,7 +2,7 @@ import {
   AT_LEAST_ONE_NUMBER_REGEX,
   EMAIL_REGEX,
   NO_SPACES_REGEX,
-  UPPERCASE_AND_LOWERCASE_REGEX,
+  AT_LEAST_ONE_UPPERCASE_REGEX,
 } from "constants/regex";
 import { useTranslation } from "react-i18next";
 import * as yup from "yup";
@@ -19,8 +19,8 @@ const useAuthorizationSchema = () => {
       .min(8, t("fields.password.validation.8orMoreCharacters"))
       .max(64, t("fields.password.validation.64orLessCharacters"))
       .matches(
-        UPPERCASE_AND_LOWERCASE_REGEX,
-        t("fields.password.validation.uppercaseAndLowercase"),
+        AT_LEAST_ONE_UPPERCASE_REGEX,
+        t("fields.password.validation.atLeastOneUppercaseLetter"),
       )
       .matches(
         AT_LEAST_ONE_NUMBER_REGEX,
